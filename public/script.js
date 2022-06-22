c8d8531e4007fb765d7daeb7b05640dff0e0d505
Samuel Ujazda <samuelujazda@gmail.com>
2022-06-22 17:30:05 +0200

    setting up page

---
+++ public/script.js
@@ -0,0 +1,5 @@
+const map = L.map('map').setView([0, 0], 1);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
